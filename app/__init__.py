4f7b5a22873d284d028a9ad279a8bfea66a969a6
Edmartt <47486245+Edmartt@users.noreply.github.com>
2023-10-30 16:15:07 -0500

    chore: adds swagger objects

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_swagger_ui import get_swaggerui_blueprint
 from config import config
 
 db = SQLAlchemy()
 
 def create_app(config_name: str) -> Flask:
     app = Flask(__name__)
+    SWAGGER_URL = '/api/docs'
+    API_URL = '/static/swagger.yml'
+
+    swagger_blueprint = get_swaggerui_blueprint(
+            SWAGGER_URL,
+            API_URL,
+            config={
+                'work orders': 'search'
+                }
+            )
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
     db.init_app(app)
@@ -15,5 +26,6 @@ def create_app(config_name: str) -> Flask:
 
     app.register_blueprint(customer_bp)
     app.register_blueprint(orders_bp)
+    app.register_blueprint(swagger_blueprint)
 
     return app
