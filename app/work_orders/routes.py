f61974263a1b4faa141548309403f3fd44baa628
Edmartt <47486245+Edmartt@users.noreply.github.com>
2023-11-02 17:38:38 -0500

    chore: deletes use of separated class for updating orders status

--- app/work_orders/routes.py
+++ app/work_orders/routes.py
@@ -1,5 +1,5 @@
 from app.models import Customer, WorkOrder
-from app.work_orders.http_orders import OrdersHTTP, HTTPOrderID, StatusOrderChange
+from app.work_orders.http_orders import OrdersHTTP, HTTPOrderID
 from . import orders_bp
 
 order = WorkOrder()
@@ -7,12 +7,8 @@ customer = Customer()
 
 orders_http = OrdersHTTP.as_view('orders', order, customer)
 order_get_id = HTTPOrderID.as_view('by id')
-order_status_change = StatusOrderChange.as_view('change')
-
-orders_bp.add_url_rule('/api/v1/orders', view_func=orders_http, methods=['POST'])
-
-orders_bp.add_url_rule('/api/v1/orders/<string:id>', view_func=order_get_id, methods=['GET'])
 
 orders_bp.add_url_rule('/api/v1/orders', view_func=orders_http, methods=['GET'])
-
-orders_bp.add_url_rule('/api/v1/orders/status', view_func=order_status_change, methods=['POST'])
+orders_bp.add_url_rule('/api/v1/orders/<string:id>', view_func=order_get_id, methods=['GET'])
+orders_bp.add_url_rule('/api/v1/orders', view_func=orders_http, methods=['POST'])
+orders_bp.add_url_rule('/api/v1/orders', view_func=orders_http, methods=['PUT'])
