0a775c3999743ce6b2ab6b95d4e427fb575b9074
Edmartt <47486245+Edmartt@users.noreply.github.com>
2023-11-17 14:19:23 -0500

    refactor: changes Customer to CustomerORM class

--- app/customers/http_customer.py
+++ app/customers/http_customer.py
@@ -1,6 +1,6 @@
 from flask import jsonify, request
 from flask.views import MethodView
-from app.models import Customer
+from app.models import CustomerORM
 from app import db
 from app.security import auth_decorator
 from app.customers.errors.errors import not_authorized
@@ -9,12 +9,12 @@ class CustomerHTTP(MethodView):
 
     decorators = [auth_decorator]
 
-    def __init__(self, customer: Customer) -> None:
+    def __init__(self, customer: CustomerORM) -> None:
         self.customer = customer
 
     #customer by active status
     def get(self):
-        active_customer_list_objects = Customer.query.filter(Customer.is_active).all()
+        active_customer_list_objects = CustomerORM.query.filter(CustomerORM.is_active).all()
         active_users =[]
 
         for user in active_customer_list_objects:
